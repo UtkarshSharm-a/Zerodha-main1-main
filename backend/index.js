8f2382900c0d62c17157dcaca59c8a8ac1088856
UtkarshSharm-a <158783756+UtkarshSharm-a@users.noreply.github.com>
2025-08-11 22:15:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
   res.send("Server is running!");
 });
 
-const allowedOrigins = ["http://localhost:5173", "http://localhost:3000"];
+const allowedOrigins = ["https://stock-buy-sell-frontends.onrender.com", "https://stock-buy-sell-dashbords.onrender.com"];
 
 app.use(cors({
   origin: function (origin, callback) {
