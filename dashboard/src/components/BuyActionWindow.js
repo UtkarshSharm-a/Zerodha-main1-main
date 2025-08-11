259cfec98b78c5002d0d30f967f909e953993b83
UtkarshSharm-a <158783756+UtkarshSharm-a@users.noreply.github.com>
2025-08-11 23:54:12 +0530

    Update BuyActionWindow.js

--- dashboard/src/components/BuyActionWindow.js
+++ dashboard/src/components/BuyActionWindow.js
@@ -20,7 +20,7 @@ const handleBuyClick = async () => {
     mode: "BUY",
   });
   try {
-    await axios.post("http://https://stock-buy-sell-backend.onrender.com/newOrder", {
+    await axios.post("http://stock-buy-sell-backend.onrender.com/newOrder", {
       name: uid,
       qty: stockQuantity,
       price: stockPrice,
